67dea8b188ef23fda789de8122422feb8ae1137e
Dmitriy_Demin <d.demin@inbox.ru>
2024-01-26 17:34:32 +0400

    sqr

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace seminar
+{
+    class Programm
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("введите первое целое число");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("введите второе целое число");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num2*num2==num1)
+            {
+            System.Console.WriteLine("да"); 
+            }  
+            else
+            System.Console.WriteLine("нет ");    
+        }
+    }
+}
