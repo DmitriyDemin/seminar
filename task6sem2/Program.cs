316ef830c83f3f6c8374f0b3ebd37091ebeca3b7
Dmitriy_Demin <d.demin@inbox.ru>
2024-01-31 18:38:01 +0400

    rename all folders

---
+++ task6sem2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 3: Напишите программу, которая принимает
+// на вход целое число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+using System;
+using System.Diagnostics;
+namespace task6sem2
+{
+    class Numbers
+    {
+        public static void Main()
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            int num;
+            Console.Clear();
+            {
+                Console.Write("введите целое число из отрезка [10, 99]:  ");
+                num = int.Parse(Console.ReadLine()!);
+                while (num < 10 || num > 99)
+                {
+                    Console.Write("ошибка ввода!\nвведите целое число из отрезка [10, 99]:  ");
+                    num = int.Parse(Console.ReadLine()!);
+                }
+            }
+            int num1 = num / 10;
+            int num2 = num % 10;
+            if (num1 > num2)
+                Console.WriteLine($"ннаибольшая цифра числа {num}: {num1}");
+            else
+                Console.WriteLine($"ннаибольшая цифра числа {num}: {num2}");
+            timer.Stop();
+            Console.WriteLine("-{0}", timer.Elapsed);
+        }
+    }
+}
---
+++ task7sem2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 4: Напишите программу, которая на вход
+// принимает натуральное число N, а на выходе
+// показывает его цифры через запятую
+using System;
+using System.Diagnostics;
+namespace task7sem2
+{
+    class Numbers
+    {
+        public static void Main()
+        {
+            Stopwatch timer = new Stopwatch();
+            
+            int num;
+            int num1;
+            int num2;
+            Console.Clear();
+            {
+                Console.Write("введите натуральное число N:  ");
+                num = int.Parse(Console.ReadLine()!);
+                while (num <= 0)
+                {
+                    Console.Write("ошибка ввода!\nвведите натуральное число N:  ");
+                    num = int.Parse(Console.ReadLine()!);
+                }
+                while (num > 10)
+                {
+                    num1 = num / 10;
+                    num2 = num - (num1*10);
+                    num = num1;
+                    
+                    Console.Write($"{num2}, ");
+                    
+                }
+                
+                Console.Write($"{num} ");
+            }
+        }
+    }
+}
