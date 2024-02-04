4c23f0d5a4fd3b8a3ca82fea64b3f1a0310bd5bc
Dmitriy_Demin <d.demin@inbox.ru>
2024-02-04 16:21:44 +0400

    task4

--- seminar_3/task4_sem3/Program.cs
+++ seminar_3/task4_sem3/Program.cs
@@ -1,3 +1,22 @@
 ﻿// Дано натуральное трёхзначное число. Создайте массив, состоящий из
 // цифр этого числа. Младший разряд числа должен располагаться на 0-
 // м индексе массива, старший – на 2-м. 
+// Console.Write("Введите натурольное трехзначное число: ");
+// string number = Console.ReadLine()!;
+// int n = int.Parse(Console.ReadLine()!);
+// int arrey = new int(number.Length);
+
+// for (int i=0; i<number.Length; i++)
+// {
+//     if 
+// }
+//     if
+
+
+Console.Clear();
+int n = 456;
+int[] array = new int[3];
+array[0] = n % 10;
+array[1] = (n % 100) / 10;
+array[2] = n / 100;
+Console.WriteLine($"[{string.Join(" ", array)}]");
