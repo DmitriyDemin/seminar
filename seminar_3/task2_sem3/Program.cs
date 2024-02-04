157cc1f2c9743312dc5f00153a5fa878c5863114
Dmitriy_Demin <d.demin@inbox.ru>
2024-02-04 15:42:20 +0400

    3семинар2задача

--- seminar_3/task2_sem3/Program.cs
+++ seminar_3/task2_sem3/Program.cs
@@ -2,3 +2,27 @@
 // промежутка [-10, 10]. Замените отрицательные элементы на
 // положительные, а положительные на отрицательные.
 
+using System;
+namespace task2_sem3;
+class Programm
+{
+    public static void Main()
+    {
+        Console.Clear();
+        Console.Write("Введите количество элементов массива: ");
+        int n = int.Parse(Console.ReadLine()!);
+        int[] array = new int[n];
+        for (int i = 0; i < array.Length; i++) // можно сразу сослаться на 'n' но лучше length
+            array[i] = new Random().Next(-10, 11);
+        Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] < 0)
+                array[i] = -array[i];
+            else if (array[i] > 0)
+                array[i] = -array[i];
+        }
+        Console.WriteLine($"Преобразованный массив: [{string.Join(", ", array)}]");
+    }
+}
+
